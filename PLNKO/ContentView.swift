3fa74a6eece20f37737447887fe59f8d3832dde2
Alexander <oBelyash@yandex.ru>
2025-02-01 17:42:52 +0300

    Initial Commit

---
+++ PLNKO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PLNKO
+//
+//  Created by Александр Андреев on 01.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PLNKO/PLNKOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLNKOApp.swift
+//  PLNKO
+//
+//  Created by Александр Андреев on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PLNKOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
